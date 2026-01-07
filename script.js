9545fcd38306431d8c0912b9a1dfc1c523b2e669
Uriyann <joshuabarotea06@gmail.com>
2026-01-07 20:04:04 +0800

    added userinfo const

--- script.js
+++ script.js
@@ -5,3 +5,8 @@
 // Sidebar Navigation
 const sidebar = document.getElementById('sidebar');
 const toggleSideBar = document.getElementById('toggleSideBar');
+
+// User Info
+const randomGreeting = document.getElementById('randomGreeting');
+const username = document.getElementById('username');
+const currentDate = document.getElementById('currentDate');
