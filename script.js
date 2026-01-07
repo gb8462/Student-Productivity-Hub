7a174e2888f10ca964a133de95e90091ccde09a7
Uriyann <joshuabarotea06@gmail.com>
2026-01-07 20:04:23 +0800

    added dashboard const

--- script.js
+++ script.js
@@ -10,3 +10,11 @@ const toggleSideBar = document.getElementById('toggleSideBar');
 const randomGreeting = document.getElementById('randomGreeting');
 const username = document.getElementById('username');
 const currentDate = document.getElementById('currentDate');
+
+// Dashboard Section
+const randomFocus = document.getElementById('randomFocus');
+const taskBox = document.getElementById('taskBox');
+const taskCount = document.getElementById('taskCount');
+const timeBox = document.getElementById('timeBox');
+const modeBox = document.getElementById('modeBox');
+const randomQuoteText = document.getElementById('randomQuoteText');
