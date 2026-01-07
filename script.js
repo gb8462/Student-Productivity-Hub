d0dd150b609dd5abe90000ba8c30b21cc29f463c
Uriyann <joshuabarotea06@gmail.com>
2026-01-07 22:15:50 +0800

    done with shit for now

--- script.js
+++ script.js
@@ -66,7 +66,8 @@ function dateToday() {
     // Date Const
     const dateToday = new Date();
 
-    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", 
+                    "Friday", "Saturday"]
     const months = ["January", "February", "March", "April", "May", "June", 
                     "July", "August", "September", "October", "November", 
                     "December"]
