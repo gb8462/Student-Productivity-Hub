3d8b17f19fbb652373747963ab4defdfb01ad8d7
Uriyann <joshuabarotea06@gmail.com>
2026-01-08 19:04:00 +0800

    added tasks js

---
+++ src/js/tasks.js
@@ -0,0 +1,5 @@
+// Task List Section
+const newTaskButton = document.getElementById('newTaskButton');
+const editTaskButton = document.getElementById('editTaskButton');
+const deleteTaskButton = document.getElementById('deleteTaskButton');
+const addTableButton = document.getElementById('addTableButton');
