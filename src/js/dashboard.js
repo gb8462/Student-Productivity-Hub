3193505222a227824e0d67b571e273978a16fe9c
Uriyann <joshuabarotea06@gmail.com>
2026-01-08 19:01:26 +0800

    added dash js

---
+++ src/js/dashboard.js
@@ -0,0 +1,9 @@
+// Dashboard Section
+const randomFocus = document.getElementById('randomFocus');
+const taskBox = document.getElementById('taskBox');
+const taskCount = document.getElementById('taskCount');
+const timeBox = document.getElementById('timeBox');
+const modeBox = document.getElementById('modeBox');
+const randomQuoteText = document.getElementById('randomQuoteText');
+const quoteAuthor = document.getElementById('quoteAuthor');
+
