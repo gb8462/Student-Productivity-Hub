7611cc08960b22307266b0886306b93340397cf7
Uriyann <joshuabarotea06@gmail.com>
2026-01-10 20:44:35 +0800

    Reorganize task section comments for improved code clarity

--- src/js/tasks.js
+++ src/js/tasks.js
@@ -1,5 +1,10 @@
 // Task List Section
+
+// Buttons
 const newTaskButton = document.getElementById('newTaskButton');
 const editTaskButton = document.getElementById('editTaskButton');
 const deleteTaskButton = document.getElementById('deleteTaskButton');
-const addTableButton = document.getElementById('addTableButton');
+
+// Task List
+const taskInput = document.getElementById('taskInput');
+const taskList = document.getElementById('taskList');
