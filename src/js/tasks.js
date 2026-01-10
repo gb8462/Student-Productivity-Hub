9d70f4a367abf82904df37b21dde1109dbbd8545
Uriyann <joshuabarotea06@gmail.com>
2026-01-10 20:57:12 +0800

    Rename 'newTaskButton' to 'addTaskButton' for improved clarity in task management

--- src/js/tasks.js
+++ src/js/tasks.js
@@ -1,9 +1,13 @@
 // Task List Section
 
 // Buttons
-const newTaskButton = document.getElementById('addTaskButton');
+const addTaskButton = document.getElementById('addTaskButton');
 const editTaskButton = document.getElementById('editTaskButton');
 const deleteTaskButton = document.getElementById('deleteTaskButton');
+
+// Progress Bar
+const progressLabel = document.getElementById('progressLabel')
+const progressBar = document.getElementById('progressFill')
 
 // Task List
 const taskInput = document.getElementById('taskInput');
