c13e2ab196d6185f60bfb12c510cedf2b0391013
Uriyann <joshuabarotea06@gmail.com>
2026-01-12 00:04:04 +0800

    finished lighttheme switch js

--- src/js/theme-switch.js
+++ src/js/theme-switch.js
@@ -1,4 +1,25 @@
 // Theme Switch JS
 
-let darkmode = localStorage.getItem('darkmode')
+let darkmode = localStorage.getItem('dark-theme');
 const themeSwitch = document.getElementById('themeSwitch');
+
+const enableDarkMode = () => {
+    document.body.classList.add('dark-theme');
+    localStorage.setItem('dark-theme', 'active');
+};
+
+const disableDarkMode = () => {
+    document.body.classList.remove('dark-theme');
+    localStorage.setItem('dark-theme', null);
+};
+
+if(darkmode === 'active') enableDarkMode();
+
+themeSwitch.addEventListener('click', () => {
+    darkmode = localStorage.getItem('dark-theme');
+    if (darkmode !== "active") {
+        enableDarkMode();
+    } else {
+        disableDarkMode();
+    }
+});
