000d7d83078100aab56730e387a90d4b04955e4e
Uriyann <joshuabarotea06@gmail.com>
2026-01-15 20:21:17 +0800

    renamed script

---
+++ src/script/tasks.js
@@ -0,0 +1,15 @@
+// Task List Section
+
+// Buttons
+const addTaskButton = document.getElementById('addTaskButton');
+const editTaskButton = document.getElementById('editTaskButton');
+const deleteTaskButton = document.getElementById('deleteTaskButton');
+
+// Progress Bar
+const progressLabel = document.getElementById('progressLabel')
+const progressFill = document.getElementById('progressFill')
+
+// Task List
+const taskInput = document.getElementById('taskInput');
+const taskList = document.getElementById('taskList');
+
