d37b995ac913d4e5932650d2a2474778ff4759db
Uriyann <joshuabarotea06@gmail.com>
2026-01-18 22:39:05 +0800

    successfully added stats progress function and an event listener to add task button

--- src/script/tasks.js
+++ src/script/tasks.js
@@ -1,5 +1,4 @@
 // Task List Section
-
 // Buttons
 const addTaskButton = document.getElementById('addTaskButton');
 const editTaskButton = document.getElementById('editTaskButton');
@@ -9,7 +8,51 @@ const deleteTaskButton = document.getElementById('deleteTaskButton');
 const progressLabel = document.getElementById('progressLabel')
 const progressFill = document.getElementById('progressFill')
 
-// Task List
+// Task Lists
 const taskInput = document.getElementById('taskInput');
 const taskList = document.getElementById('taskList');
 
+
+let selectedTask = null;
+
+/* ----------- Progress Update Function ----------- */
+function statsProgress() {
+    const tasks = taskList.querySelectorAll('li');
+    const finishedTasks = taskList.querySelectorAll('input:checked').length;
+
+    progressLabel.textContent = `${finishedTasks} of ${tasks.length} done`;
+    progressFill.value = tasks.length === 0 ? 0 : (finishedTasks / tasks.length) * 100;
+}
+
+/* ----------- Add Task Function ----------- */
+addTaskButton.addEventListener('click', () => {
+    const checkTaskInput = taskInput.value.trim();
+    if (!checkTaskInput) return;
+
+    const createList = document.createElement('li');
+    createList.classList.add('task-item');
+
+    const createCheckBox = document.createElement('input');
+    createCheckBox.type = 'checkbox'
+
+    const createSpan = document.createElement('span');
+    createSpan.classList.add('task-text');
+    createSpan.innerHTML = checkTaskInput;
+
+    createList.addEventListener('click', () => {
+        document.querySelectorAll('task-item').forEach(t => t.classList.remove('selected'));
+        createList.classList.add('selected');
+        selectedTask = createList;
+    });
+
+    createCheckBox.addEventListener('change', () => {
+        statsProgress();
+    });
+
+    taskList.appendChild(createList);
+    createList.appendChild(createCheckBox);
+    createList.appendChild(createSpan);
+
+    taskInput.value = '';
+    statsProgress();
+});
