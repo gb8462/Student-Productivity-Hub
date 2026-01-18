2733d505977fbc6a88c0f1fa2c3aade99dba7e49
Uriyann <joshuabarotea06@gmail.com>
2026-01-18 13:51:02 +0800

    emptied js

--- src/script/authentication.js
+++
@@ -1,7 +0,0 @@
-const registerBtn = document.getElementById('switchRegister');
-const authForm = document.querySelector('.auth-form');
-
-registerBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    authForm.classList.add("show-register");
-})
