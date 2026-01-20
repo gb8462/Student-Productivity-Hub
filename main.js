4efc2dad45d1a24b694c38c690fec49bf23d192d
Uriyann <joshuabarotea06@gmail.com>
2026-01-20 20:03:29 +0800

    adding carousel auto scroll function js

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
-/* 
-    Hero Fade Function
-*/
+/* ================================
+            Variables
+================================ */
 
+// Hero Text Group
 const heroFadeGroup = document.querySelector('.fade-hero-contents');
+
+// Carousel lists
+const carouselTrack = document.querySelector('.carousel-list ul');
+const carouselItems = document.querySelectorAll('.items');
+
+
+
+/* ================================
+            Scroll Function
+================================ */
 
 window.addEventListener('scroll', () => {
     const scrollY = window.scrollY;
@@ -15,3 +26,7 @@ window.addEventListener('scroll', () => {
     heroFadeGroup.style.opacity = 1 - progress;
     heroFadeGroup.style.transform = `scale(${1 - progress * 0.15}) translateY(${progress * -40}px)`;
 })
+
+/* ================================
+            Scroll Function
+================================ */
