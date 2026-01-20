f47817d9f036a299ed42a70892dfe0a9a4840c7d
Uriyann <joshuabarotea06@gmail.com>
2026-01-20 19:08:16 +0800

    added script js for faded group

---
+++ main.js
@@ -0,0 +1,17 @@
+/* 
+    Hero Fade Function
+*/
+
+const heroFadeGroup = document.querySelector('.fade-hero-contents');
+
+window.addEventListener('scroll', () => {
+    const scrollY = window.scrollY;
+    const fadeStart = 90;
+    const fadeEnd = 600;
+
+    let progress = (scrollY - fadeStart) / (fadeEnd - fadeStart);
+    progress = Math.min(Math.max(progress, 0), 1);
+
+    heroFadeGroup.style.opacity = 1 - progress;
+    heroFadeGroup.style.transform = `scale(${1 - progress * 0.15}) translateY(${progress * -40}px)`;
+})
